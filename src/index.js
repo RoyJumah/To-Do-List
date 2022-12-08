91b27b316518c53ce87c8ef2d6aa32e738adad91
RoyJumah <royjumah17@gmail.com>
2022-12-08 13:17:12 +0300

    Add function to display and edit todo task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
@@ -5,44 +6,7 @@ import {
   Store,
   displayTodo,
   createTodo,
-  deleteTodo,
   updateTodos,
 } from './modules/liststore.js';
 
 import './style.css';
-
-const inputTodo = document.getElementById('new-todo');
-const submitTodo = document.getElementById('submit');
-const todo = Store.getTodos();
-const sortedTodo = [...todo].sort((prev, next) => prev.index - next.index);
-inputTodo.addEventListener('keypress', e => {
-  if (e.key === 'Enter') {
-    e.preventDefault();
-    // trigger form submit
-    submitTodo.click();
-  }
-});
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  createTodo();
-});
-document.addEventListener('DOMContentLoaded', () => {
-  sortedTodo.forEach(todo => {
-    displayTodo(todo);
-  });
-
-  // Event: delete todo
-  form.addEventListener('click', e => {
-    const clicked = e.target.closest('.delete');
-    if (!clicked) return;
-    // // Remove from ui
-    deleteTodo(clicked);
-    // Remove from local storage
-    Store.deleteTodo(clicked.dataset.del);
-  });
-  form.addEventListener('click', e => {
-    const clicked = e.target.closest('.todo-item');
-    if (!clicked) return;
-    updateTodos(clicked);
-  });
-});
--- src/modules/liststore.js
+++ src/modules/liststore.js
@@ -1,61 +1,34 @@
-class Store {
-  // get todo items from local storage
-  static getTodos = () => {
-    let todos;
-    if (localStorage.getItem('todos') === null) {
-      todos = [];
-    } else {
-      todos = JSON.parse(localStorage.getItem('todos'));
-    }
-    return todos;
-  };
-  // add todo item to local storage
-  static addTodo = todo => {
-    const todos = this.getTodos();
-    todos.push(todo);
-    localStorage.setItem('todos', JSON.stringify(todos));
-  };
-  //update todos
-  static updateTodos = todos => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  };
-  // remove todo item from local storage
-  static deleteTodo = ind => {
-    const todos = this.getTodos();
-    const newTodos = todos.forEach((todo, index) => {
-      if (todo.index === ind) {
-        todos.splice(index, 1);
-      }
-    });
-
-    localStorage.setItem('todos', JSON.stringify(newTodos));
-  };
-}
-const form = document.getElementById('form');
-const displayTodo = ({ description, index }) => {
-  const listItem = document.createElement('div');
-  listItem.className = 'row list-item';
-  listItem.innerHTML = `
-    <span class="icon-check"></span>
+const listContainer = document.querySelector('.list-container');
+const displayTodo = () => {
+  listContainer.innerHTML = '';
+  const todos = Store.getTodos();
+  todos.forEach(({ description, index }) => {
+    const listItem = document.createElement('div');
+    listItem.className = `row list-item list-item-${index}`;
+    listItem.innerHTML = `
+    <i class="fa-regular fa-square"></i>
     <input class="todo-item" type="text" data-todo="${index}" value=${description} />
-    <i class="fa-solid fa-trash-can delete" data-del="${index}"></i>
+    
+    
+    <button class="delete" data-del="${index}"><i class="fa-solid fa-trash-can" ></i></button>
     <svg
       width="25px"
       height="30px"
       class="icon-move"
-      xmlns="
-http://www.w3.org/2000/svg
-"
+      xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
     >
       <path
         d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
-        fill="#B6B6B8"
+        fill="#b6b6b8"
       ></path>
+
     </svg>
   `;
-  form.appendChild(listItem);
+    listContainer.appendChild(listItem);
+  });
 };
+
 const createTodo = () => {
   // get todo description
   const description = document.querySelector('#new-todo').value;
@@ -64,15 +37,13 @@ const createTodo = () => {
   const todo = {
     description: `${description}`,
     completed: false,
-    index: `${Store.getTodos().length + 1}`,
+    index: `${Store.getTodos().length}`,
   };
-  // add new todo to UI
-  displayTodo(todo);
   // add new todo to local storage
   Store.addTodo(todo);
-};
-const deleteTodo = el => {
-  el.parentElement.remove();
+  form.reset();
+  // add new todo to UI
+  displayTodo();
 };
 const updateTodos = el => {
   el.addEventListener('keyup', () => {
@@ -83,5 +54,3 @@ const updateTodos = el => {
     Store.updateTodos(todos);
   });
 };
-// localStorage.clear();
-export { Store, displayTodo, createTodo, deleteTodo, updateTodos };
