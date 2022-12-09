518d6f5c261e94b2c3c5da4386a63c3a31f0ba79
RoyJumah <royjumah17@gmail.com>
2022-12-09 16:26:47 +0300

    Create function to create a line through tasks once checkbox is clicked

--- src/index.js
+++ src/index.js
@@ -18,3 +18,32 @@ import './style.css';
 const form = document.getElementById('form');
 const listContainer = document.querySelector('.list-container');
 const clear = document.querySelector('.clear');
+
+window.addEventListener('load', displayTodo);
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  createTodo();
+});
+
+// Event: delete todo
+listContainer.addEventListener('click', e => {
+  const clicked = e.target.closest('.delete');
+  if (!clicked) return;
+
+  Store.deleteTodo(+clicked.dataset.del);
+  displayTodo();
+});
+
+listContainer.addEventListener('click', e => {
+  const clicked = e.target.closest('.todo-item');
+  if (!clicked) return;
+
+  updateTodos(clicked);
+});
+listContainer.addEventListener('click', e => {
+  const clicked = e.target.closest('.check-box');
+  if (!clicked) return;
+
+  updateStatus(clicked);
+});
+clear.addEventListener('click', clearCompleted);
